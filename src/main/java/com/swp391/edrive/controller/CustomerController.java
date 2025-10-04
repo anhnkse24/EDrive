4389fa9ae0c4265d9720aa9a38ee40ce86102daf
Kin <tahoangnhat@gmail.com>
2025-10-04 07:57:13 +0700

    [FEAT] bổ sung chức năng cho customer

--- src/main/java/com/swp391/edrive/controller/CustomerController.java
+++ src/main/java/com/swp391/edrive/controller/CustomerController.java
@@ -6,10 +6,7 @@ import com.swp391.edrive.dto.response.ResponseObject;
 import com.swp391.edrive.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/customer")
@@ -21,5 +18,27 @@ public class CustomerController {
     public ResponseEntity<ResponseObject> createCustomer(@RequestBody CustomerRequest request) {
         CustomerResponse res = customerService.createCustomer(request);
         return ResponseEntity.ok(new ResponseObject(200, "Customer created successfully", res));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<ResponseObject> updateCustomer(
+            @PathVariable("id") Long id,
+            @RequestBody CustomerRequest request) {
+        CustomerResponse res = customerService.updateCustomer(id, request);
+        return ResponseEntity.ok(new ResponseObject(200, "Customer updated successfully", res));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResponseObject> deleteCustomer(@PathVariable("id") Long id) {
+        customerService.deleteCustomer(id);
+        return ResponseEntity.ok(new ResponseObject(200, "Customer deleted successfully", null));
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseObject> getCustomerById(@PathVariable("id") Long id) {
+        CustomerResponse res = customerService.getCustomerById(id);
+        return ResponseEntity.ok(new ResponseObject(200, "Customer retrieved successfully", res));
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseObject> getAllCustomers() {
+        return ResponseEntity.ok(new ResponseObject(200, "Customers retrieved successfully", customerService.getAllCustomers()));
     }
 }
