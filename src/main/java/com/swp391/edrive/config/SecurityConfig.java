ba69280a9266fe89063755438f48d26969208826
anhnkse24 <kyanhngo521@gmail.com>
2025-10-24 22:40:35 +0700

    update(SecurityConfig)

--- src/main/java/com/swp391/edrive/config/SecurityConfig.java
+++ src/main/java/com/swp391/edrive/config/SecurityConfig.java
@@ -73,6 +73,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/vehicles/**").permitAll()
                         .requestMatchers(HttpMethod.DELETE, "/api/vehicles/**").permitAll()
                         .requestMatchers(HttpMethod.PUT, "/api/vehicles/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/dealers/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/dealers/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form.disable())
