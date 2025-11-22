af5f3807070b9e03a4a54fedfb14e0cd7f5e4982
Kin <tahoangnhat@gmail.com>
2025-11-22 16:30:28 +0700

    [add]thêm thông báo khi dealermanager chuyển trạng thái testdrive

--- src/main/java/com/swp391/edrive/service/NotificationService.java
+++ src/main/java/com/swp391/edrive/service/NotificationService.java
@@ -1,8 +1,10 @@
 package com.swp391.edrive.service;
 
+import com.swp391.edrive.dto.request.TestDriveStatusRequest;
 import com.swp391.edrive.dto.response.NotificationResponse;
 import com.swp391.edrive.entity.Contract;
 import com.swp391.edrive.entity.Order;
+import com.swp391.edrive.entity.TestDrive;
 
 import java.util.List;
 
@@ -18,5 +20,5 @@ public interface NotificationService {
     void createNotificationForDeliveryConfirmed(Order order);
     List<NotificationResponse> getNotificationsForDealerManager(Long dealerId);
     List<NotificationResponse> getNotificationsForDealerStaff(Long dealerId);
-
-    }
+    void createNotificationForTestDriveStatusForStaff(TestDrive testDrive, TestDriveStatusRequest request);
+}
